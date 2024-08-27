d08b0445c4e44f1490f9381ee39ce110392f127a
JesusM15 <guerrajesusm72@gmail.com>
2024-08-26 17:59:13 -0700

    host

--- social_api/settings.py
+++ social_api/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    
+    'https://ejchat-api.onrender.com',
 ]
 
 
@@ -56,6 +56,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # URL de tu frontend,
     'http://127.0.0.1:8000',
+    'https://ejchat-api.onrender.com',
     # Otras URLs permitidas si es necesario
 ]
 
