390624e8db724f62bcc70b4009e9621fec768928
JesusM15 <guerrajesusm72@gmail.com>
2024-08-26 19:02:56 -0700

    re orden

--- social_api/asgi.py
+++ social_api/asgi.py
@@ -1,15 +1,19 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_api.settings')
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_api.settings')
+
+django_application = get_asgi_application()
+
 from social_api.routing import websocket_urlpatterns
 from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
-from channels.security.websocket import AllowedHostsOriginValidator
 
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_application,
     "websocket": AllowedHostsOriginValidator
         (AuthMiddlewareStack(
             URLRouter(
